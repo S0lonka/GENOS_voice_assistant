a0b3da5ce705f0d1debcaa78beb5c39ab7cb143c
S0lonka <solowayplay@gmail.com>
2025-06-08 19:16:58 +0300

    imports

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from pvrecorder import PvRecorder
+import pvporcupine
+import vosk
+from vosk import Model, KaldiRecognizer
 
+
+helper_name_path = "helpername/genas_en_windows_v3_0_0.ppn"
 def main():
     print("start work")
-    
+
 if __name__ == "__main__":
     main()
